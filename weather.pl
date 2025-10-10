437f02e2d1f77776d75c921e73249f2974ae0a4f
Jory A. Pratt <geekypenguin@gmail.com>
2025-10-09 21:06:34 -0500

    Bump version to 2.7.1
    - Updated VERSION to 2.7.1 in weather.pl and saytime.pl
    - Added debian/changelog entry for 2.7.1
    - Includes all timezone fixes and default_country feature
    - Ready for clean build from git

--- weather.pl
+++ weather.pl
@@ -50,7 +50,7 @@ use constant {
     TEMP_FILE => "/tmp/temperature",
     COND_FILE => "/tmp/condition.ulaw",
     TIMEZONE_FILE => "/tmp/timezone",
-    VERSION => '2.7.0',
+    VERSION => '2.7.1',
     WEATHER_SOUND_DIR => "/usr/share/asterisk/sounds/en/wx",
 };
 
